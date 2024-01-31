8286fcf69c8f1186247f341fa38f77cc04a343c5
Karandeep Singh <karan31q@gmail.com>
2024-01-31 14:00:35 +0530

    use card layout for setup screen
    * also move hardcoded text to string resource
    * also refractor empty username string

--- app/src/main/java/com/imarti/affirmations/SetupScreen.kt
+++ app/src/main/java/com/imarti/affirmations/SetupScreen.kt
@@ -2,10 +2,12 @@ package com.imarti.affirmations
 
 import android.content.Context
 import android.widget.Toast
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -18,6 +20,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,73 +36,59 @@ fun SetupScreen(navController: NavHostController) {
         mutableStateOf(sharedPrefs.getString("user_name", "") ?: "User")
     }
     // Same as the bar in main screen (idk if i should add or not)
-    /*
-    Row (
-        Modifier.padding(start = 14.dp, end = 14.dp, top = 14.dp)
-    ){
-        Row (
+    Column(
+        modifier = Modifier.padding(start = 14.dp, end = 14.dp, top = 14.dp)
+    ) {
+        Column(
             modifier = Modifier
-                .fillMaxWidth()
+                .padding(bottom = 2.dp)
+                .fillMaxSize()
                 .background(
-                    color = MaterialTheme.colorScheme.primaryContainer,
+                    color = MaterialTheme.colorScheme.secondaryContainer,
                     shape = RoundedCornerShape(22.dp)
                 ),
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
-                text = "Daily Affirmations",
+                text = stringResource(id = R.string.setup_screen_text),
+                fontFamily = HarmonyOS_Sans,
                 modifier = Modifier.padding(14.dp),
-                color = MaterialTheme.colorScheme.onPrimaryContainer,
-                style = MaterialTheme.typography.headlineSmall,
-                fontFamily = HarmonyOS_Sans
+                color = MaterialTheme.colorScheme.onSecondaryContainer,
+                style = MaterialTheme.typography.titleMedium,
+                textAlign = TextAlign.Center
             )
-        }
-    }
-    */
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = "Welcome User!\nPlease update the following according to your preference",
-            fontFamily = HarmonyOS_Sans,
-            modifier = Modifier.padding(14.dp),
-            color = MaterialTheme.colorScheme.onPrimaryContainer,
-            style = MaterialTheme.typography.titleMedium,
-            textAlign = TextAlign.Center
-        )
-        OutlinedTextField(
-            modifier = Modifier.padding(14.dp),
-            value = userName,
-            onValueChange = {
-                userName = it
-            },
-        )
-        Button(
-            onClick = {
-                sharedPrefs.edit().putBoolean("first_launch", false).apply()
-                if (userName.isEmpty()) {
-                    userName = "User"
-                    Toast.makeText(context, R.string.empty_username_warning, Toast.LENGTH_SHORT).show()
-                }
-                sharedPrefs.edit().putString("user_name", userName).apply()
-                navController.navigate("main") {
-                    popUpTo(navController.graph.startDestinationId) {
-                        inclusive = true
+            OutlinedTextField(
+                modifier = Modifier.padding(14.dp),
+                value = userName,
+                onValueChange = {
+                    userName = it
+                },
+            )
+            Button(
+                onClick = {
+                    sharedPrefs.edit().putBoolean("first_launch", false).apply()
+                    if (userName.isEmpty()) {
+                        userName = "User"
+                        Toast.makeText(context, R.string.empty_username_warning, Toast.LENGTH_SHORT).show()
                     }
-                }
-            },
-            modifier = Modifier.padding(start = 14.dp)
-        ) {
-            Text(
-                text = "Done",
-                fontFamily = HarmonyOS_Sans
+                    sharedPrefs.edit().putString("user_name", userName).apply()
+                    navController.navigate("main") {
+                        popUpTo(navController.graph.startDestinationId) {
+                            inclusive = true
+                        }
+                    }
+                },
+                modifier = Modifier.padding(start = 14.dp)
+            ) {
+                Text(
+                    text = "Done",
+                    fontFamily = HarmonyOS_Sans
 
-            )
+                )
+            }
+
         }
-
     }
 }
 
