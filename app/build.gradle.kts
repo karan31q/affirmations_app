b38031b78d6b7e1a239823f580cf3a17acaabddd
Karandeep Singh <karan31q@gmail.com>
2024-02-24 18:42:12 +0530

    update minSdk to 29 (android 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.imarti.affirmations"
-        minSdk = 32
+        minSdk = 29
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
