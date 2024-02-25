4d843d376414620b51f741b60d4be63d88858258
Karandeep Singh <karan31q@gmail.com>
2024-02-25 10:10:22 +0530

    fix up navigation bar color on light theme

--- app/src/main/java/com/imarti/affirmations/ui/theme/Theme.kt
+++ app/src/main/java/com/imarti/affirmations/ui/theme/Theme.kt
@@ -60,6 +60,8 @@ fun AffirmationsTheme(
             window.statusBarColor = colorScheme.background.toArgb()
             window.navigationBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
+            WindowCompat.getInsetsController(window, view).isAppearanceLightNavigationBars = !darkTheme
+
         }
     }
 
