d946dc2765f688d02e147ea8227d8699075f1b38
Karandeep Singh <karan31q@gmail.com>
2024-03-09 10:22:06 +0530

    implement a timepicker dialog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,6 +62,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-scalars:2.9.0")
+    implementation("com.maxkeppeler.sheets-compose-dialogs:core:1.3.0")
+    implementation("com.maxkeppeler.sheets-compose-dialogs:clock:1.3.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/imarti/affirmations/SettingsPage.kt
+++ app/src/main/java/com/imarti/affirmations/SettingsPage.kt
@@ -1,19 +1,22 @@
 package com.imarti.affirmations
 
-import android.app.Activity
 import android.content.Context
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.AccountCircle
-import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -23,7 +26,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -31,6 +33,7 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -46,7 +49,12 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import com.imarti.affirmations.ui.theme.AffirmationsTheme
+import com.maxkeppeker.sheets.core.models.base.rememberUseCaseState
+import com.maxkeppeler.sheets.clock.ClockDialog
+import com.maxkeppeler.sheets.clock.models.ClockConfig
+import com.maxkeppeler.sheets.clock.models.ClockSelection
 import kotlinx.coroutines.launch
+import java.time.LocalTime
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -58,22 +66,34 @@ fun SettingsPage(navController: NavHostController) {
     var userName by rememberSaveable {
         mutableStateOf(sharedPrefs.getString("user_name", "") ?: "User")
     }
+    var hourSelected by rememberSaveable {
+        mutableIntStateOf(sharedPrefs.getInt("hour_selected", 0))
+    }
+    var minutesSelected by rememberSaveable {
+        mutableIntStateOf(sharedPrefs.getInt("minute_selected", 0))
+    }
+
+    var hourMinutesSelected by rememberSaveable {
+        mutableStateOf(LocalTime.of(hourSelected, minutesSelected))
+    }
 
     val showUserNameDialog = remember { mutableStateOf(false) }
+
     val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
 
     val scope = rememberCoroutineScope()
     val snackbarHostState = remember { SnackbarHostState() }
+    val clockState = rememberUseCaseState()
 
     // https://stackoverflow.com/questions/67401294/jetpack-compose-close-application-by-button
-    val activity = (LocalContext.current as? Activity)
+    // val activity = (LocalContext.current as? Activity)
 
     // so it can be used with coroutines
     val emptyUserNameWarning = stringResource(R.string.empty_username_warning)
     val okLabel = stringResource(R.string.got_it)
     val savedUserName = stringResource(R.string.username_updated)
-    val restartAppNotification = stringResource(R.string.restart_app_notification)
-    val restartText = stringResource(R.string.restart_action)
+    // val restartAppNotification = stringResource(R.string.restart_app_notification)
+    // val restartText = stringResource(R.string.restart_action)
     val defaultUserName = stringResource(R.string.default_username)
 
     Scaffold(
@@ -94,7 +114,7 @@ fun SettingsPage(navController: NavHostController) {
                  navigationIcon = {
                      IconButton(onClick = { navController.navigateUp() }) {
                          Icon(
-                             imageVector = Icons.Outlined.ArrowBack,
+                             imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
                              contentDescription = stringResource(R.string.settings_menu_back_button)
                          )
                      }
@@ -114,7 +134,7 @@ fun SettingsPage(navController: NavHostController) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 20.dp, bottom = 20.dp),
+                    .padding(top = 20.dp, bottom = 10.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
             ){
                 IconButton(
@@ -204,11 +224,64 @@ fun SettingsPage(navController: NavHostController) {
                     style = MaterialTheme.typography.titleLarge
                 )
             }
+
+            ClockDialog(
+                state = clockState,
+                config = ClockConfig(
+                    defaultTime = hourMinutesSelected,
+                    is24HourFormat = true
+                ),
+                selection = ClockSelection.HoursMinutes { hours, minutes ->
+                    sharedPrefs.edit().putInt("hour_selected", hours).apply()
+                    sharedPrefs.edit().putInt("minute_selected", minutes).apply()
+                    hourSelected = hours
+                    minutesSelected = minutes
+                    hourMinutesSelected = LocalTime.of(hourSelected, minutesSelected)
+                    scope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = "Selected time is $hourMinutesSelected",
+                            actionLabel = okLabel,
+                            duration = SnackbarDuration.Short
+                        )
+                    }
+
+                }
+            )
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                TextButton(
+                    colors = ButtonColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        disabledContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    contentPadding = PaddingValues(15.dp),
+                    onClick = { clockState.show() }
+                ) {
+                    Icon(
+                        Icons.Outlined.Notifications,
+                        contentDescription = "Select time for daily notifications",
+                    )
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 10.dp),
+                        text = "Select Time for Daily Notifications",
+                    )
+                }
+            }
+            /*
             Button(
                 onClick = {
                     sharedPrefs.edit().putString("user_name", "").apply()
                     sharedPrefs.edit().putBoolean("first_launch", true).apply()
-                    scope.launch { 
+                    sharedPrefs.edit().putInt("hour_selected", 12).apply()
+                    scope.launch {
                         val result = snackbarHostState
                             .showSnackbar(
                                 message = restartAppNotification,
@@ -229,6 +302,7 @@ fun SettingsPage(navController: NavHostController) {
                     text = stringResource(R.string.reset_app)
                 )
             }
+            */
         }
     }
 }
