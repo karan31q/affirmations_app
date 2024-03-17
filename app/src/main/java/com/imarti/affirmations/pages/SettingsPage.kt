57560636072e889df2e730a6de6cff8d7e9a54a4
Karandeep Singh <karan31q@gmail.com>
2024-03-17 15:10:24 +0530

    add some links in settings page
    * app source link
    * bug report link
    * add made by text that opens my portfolio site
    * display a notice that font used is harmonyos sans (required by harmonyos sans license)

--- app/src/main/java/com/imarti/affirmations/pages/SettingsPage.kt
+++ app/src/main/java/com/imarti/affirmations/pages/SettingsPage.kt
@@ -3,8 +3,11 @@ package com.imarti.affirmations.pages
 import android.app.Activity
 import android.content.Context
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -13,9 +16,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.outlined.AccountCircle
+import androidx.compose.material.icons.outlined.Build
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Refresh
+import androidx.compose.material.icons.outlined.Warning
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -32,7 +37,6 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -43,10 +47,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -65,6 +70,7 @@ fun SettingsPage(navController: NavHostController) {
     val sharedPrefs = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
     // https://stackoverflow.com/questions/67401294/jetpack-compose-close-application-by-button
     val activity = (LocalContext.current as? Activity)
+    val uriHandler = LocalUriHandler.current
 
     // so it can be used with coroutines
     val emptyUserNameWarning = stringResource(R.string.empty_username_warning)
@@ -75,6 +81,9 @@ fun SettingsPage(navController: NavHostController) {
     val defaultUserName = stringResource(R.string.default_username)
     val alarmSetMessage = stringResource(R.string.reminder_set)
     val alarmCancelledMessage = stringResource(R.string.reminder_cancelled)
+    val appSourceLink = stringResource(R.string.app_source)
+    val bugReportLink = stringResource(R.string.bug_report_link)
+    val portfolioSite = stringResource(R.string.portfolio_link)
 
     var userName by rememberSaveable {
         mutableStateOf(sharedPrefs.getString("user_name", "") ?: "User")
@@ -98,12 +107,9 @@ fun SettingsPage(navController: NavHostController) {
             }
         }
     )
-    val scrollBehavior =
-        TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
 
     Scaffold(
         modifier = Modifier
-            .nestedScroll(scrollBehavior.nestedScrollConnection)
             .statusBarsPadding(),
         topBar = {
             LargeTopAppBar(
@@ -127,7 +133,6 @@ fun SettingsPage(navController: NavHostController) {
                         )
                     }
                 },
-                scrollBehavior = scrollBehavior
             )
         },
         snackbarHost = {
@@ -349,7 +354,89 @@ fun SettingsPage(navController: NavHostController) {
                         fontFamily = HarmonyOS_Sans
                     )
                 }
+                TextButton(
+                    colors = ButtonColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        disabledContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    contentPadding = PaddingValues(15.dp),
+                    onClick = {
+                        uriHandler.openUri(appSourceLink)
+                    },
+                    modifier = Modifier.padding(bottom = 5.dp)
+                ) {
+                    Icon(
+                        Icons.Outlined.Build,
+                        contentDescription = stringResource(R.string.view_source_code)
+                    )
+                    Text(
+                        text = stringResource(R.string.view_source_code),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 10.dp),
+                        fontFamily = HarmonyOS_Sans
+                    )
+                }
+                TextButton(
+                    colors = ButtonColors(
+                        containerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        contentColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                        disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        disabledContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    contentPadding = PaddingValues(15.dp),
+                    onClick = {
+                        uriHandler.openUri(bugReportLink)
+                    },
+                    modifier = Modifier.padding(bottom = 5.dp)
+                ) {
+                    Icon(
+                        Icons.Outlined.Warning,
+                        contentDescription = stringResource(R.string.bug_report_desc)
+                    )
+                    Text(
+                        text = stringResource(R.string.bug_report),
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(start = 10.dp),
+                        fontFamily = HarmonyOS_Sans
+                    )
+                }
             }
+            Spacer(
+                modifier = Modifier.weight(1f)
+            )
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextButton(
+                    colors = ButtonColors(
+                        containerColor = MaterialTheme.colorScheme.surface,
+                        contentColor = MaterialTheme.colorScheme.onSurface,
+                        disabledContainerColor = MaterialTheme.colorScheme.surfaceVariant,
+                        disabledContentColor = MaterialTheme.colorScheme.onSurfaceVariant
+                    ),
+                    onClick = {
+                        uriHandler.openUri(portfolioSite)
+                    }
+                ){
+                    Text(
+                        text = stringResource(R.string.made_by),
+                        fontFamily = HarmonyOS_Sans,
+                        style = MaterialTheme.typography.labelSmall
+                    )
+                }
+                Text(
+                    stringResource(R.string.harmonyos_sans_notice),
+                    fontFamily = HarmonyOS_Sans,
+                    style = MaterialTheme.typography.labelSmall,
+                )
+            }
+
         }
     }
 }
