f05ad6a75f3bf4e593979ace26ba15bf1423f0f7
Karandeep Singh <karan31q@gmail.com>
2024-03-31 20:46:24 +0530

    add an background image to app
    * found this here: https://in.pinterest.com/pin/140806230791241/
    * credits of the image goes to its respective owners

--- app/src/main/java/com/imarti/affirmations/MainUI.kt
+++ app/src/main/java/com/imarti/affirmations/MainUI.kt
@@ -37,8 +37,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -153,6 +157,14 @@ fun MainUI(navController: NavHostController) {
     ) { innerPadding ->
         Column(
             modifier = Modifier
+                .paint(
+                    painterResource(id = R.drawable.app_bg),
+                    contentScale = ContentScale.FillBounds,
+                    colorFilter = ColorFilter.tint(
+                        color = MaterialTheme.colorScheme.primary
+                    ),
+                    alpha = 0.5f
+                )
                 .padding(innerPadding)
                 .fillMaxSize()
                 .padding(start = 10.dp, end = 10.dp, top = 20.dp, bottom = 20.dp),
